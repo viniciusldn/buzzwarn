6bf9f284b169282ca0b43b03f86db10b2b867873
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2022-05-31 14:03:41 -0300

    Initial commit

---
+++ src/main/java/com/trybe/acc/java/buzzwarn/App.java
@@ -0,0 +1,12 @@
+package com.trybe.acc.java.buzzwarn;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App {
+  private static final Logger LOGGING = LogManager.getLogger(App.class);
+
+  public static void main(String[] args) {
+    LOGGING.error("Este projeto é apenas para o teste!");
+  }
+}
---
+++ src/main/java/com/trybe/acc/java/buzzwarn/Logst.java
@@ -0,0 +1,19 @@
+package com.trybe.acc.java.buzzwarn;
+
+import org.apache.logging.log4j.Logger;
+
+public class Logst {
+
+  private Logger logging;
+
+  public Logst(Logger log) {
+    this.logging = log;
+  }
+
+  /**
+   * CountLogger. Insira sua lógica aqui.
+   */
+  public void countLogger() {
+
+  }
+}
