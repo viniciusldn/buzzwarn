9c07abd96a10cfd26cad4cf4a63e105f6ea55790
Vinicius Leite Dourado Nogueira <viniciusldn0@gmail.com>
2022-06-26 15:25:09 -0300

    done

--- src/main/java/com/trybe/acc/java/buzzwarn/Logst.java
+++ src/main/java/com/trybe/acc/java/buzzwarn/Logst.java
@@ -14,6 +14,21 @@ public class Logst {
    * CountLogger. Insira sua lógica aqui.
    */
   public void countLogger() {
+    int i = 1;
+    while (i <= 30) {
+      this.buzzWarn(i);
+      i++;
+    }
+  }
 
+  private void buzzWarn(int number) {
+    if (number % 5 == 0 & number % 3 == 0) {
+      logging.warn(number);
+    } else if (number % 5 == 0) {
+      logging.debug(number);
+    } else if (number % 3 == 0) {
+      logging.trace(number);
+    }
+    logging.info(number);
   }
 }
